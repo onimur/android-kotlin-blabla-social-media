e269b008875b8e792d9b504f8310c5761c7e9c32
Murillo Comino <murillo_comino@hotmail.com>
2019-10-26 18:30:18 -0300

    Adding Login and Register fragments

---
+++ app/src/main/java/com/onimus/blablasocialmedia/mvvm/ui/login/LoginFragment.kt
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * Created by Murillo Comino on 26/10/19 18:27
+ *  * Github: github.com/MurilloComino
+ *  * StackOverFlow: pt.stackoverflow.com/users/128573
+ *  * Email: murillo_comino@hotmail.com
+ *  *
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 26/10/19 18:26
+ *
+ */
+
+package com.onimus.blablasocialmedia.mvvm.ui.login
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.onimus.blablasocialmedia.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
