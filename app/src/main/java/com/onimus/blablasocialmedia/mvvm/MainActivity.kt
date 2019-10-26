7410113a6f5e2eb50ff55e0d33cebd8c6c3fed71
Murillo Comino <murillo_comino@hotmail.com>
2019-10-26 13:51:31 -0300

    Refactor MainActivity

---
+++ app/src/main/java/com/onimus/blablasocialmedia/mvvm/MainActivity.kt
@@ -0,0 +1,25 @@
+/*
+ *
+ *  * Created by Murillo Comino on 26/10/19 13:47
+ *  * Github: github.com/MurilloComino
+ *  * StackOverFlow: pt.stackoverflow.com/users/128573
+ *  * Email: murillo_comino@hotmail.com
+ *  *
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 26/10/19 13:29
+ *
+ */
+
+package com.onimus.blablasocialmedia.mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.onimus.blablasocialmedia.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
