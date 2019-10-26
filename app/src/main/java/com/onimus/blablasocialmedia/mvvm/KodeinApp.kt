996828aaf679b1dbaaf8b4ced8854420df467980
Murillo Comino <murillo_comino@hotmail.com>
2019-10-26 13:55:33 -0300

    Adding FirebaseManager

--- app/src/main/java/com/onimus/blablasocialmedia/mvvm/KodeinApp.kt
+++ app/src/main/java/com/onimus/blablasocialmedia/mvvm/KodeinApp.kt
@@ -13,12 +13,17 @@
 package com.onimus.blablasocialmedia.mvvm
 
 import android.app.Application
+import com.onimus.blablasocialmedia.mvvm.data.firebase.FirebaseManager
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
 
 class KodeinApp : Application(), KodeinAware {
     override val kodein = Kodein.lazy {
         import(androidXModule(this@KodeinApp))
+
+        bind() from singleton { FirebaseManager() }
     }
 }
---
+++ app/src/main/java/com/onimus/blablasocialmedia/mvvm/data/firebase/FirebaseManager.kt
@@ -0,0 +1,49 @@
+/*
+ *
+ *  * Created by Murillo Comino on 26/10/19 13:54
+ *  * Github: github.com/MurilloComino
+ *  * StackOverFlow: pt.stackoverflow.com/users/128573
+ *  * Email: murillo_comino@hotmail.com
+ *  *
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 26/10/19 13:54
+ *
+ */
+
+package com.onimus.blablasocialmedia.mvvm.data.firebase
+
+import com.google.firebase.auth.FirebaseAuth
+import io.reactivex.Completable
+
+class FirebaseManager {
+    private val firebaseAuth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+
+    fun login(email: String, password: String) = Completable.create { emitter ->
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (!emitter.isDisposed) {
+                if (it.isSuccessful)
+                    emitter.onComplete()
+                else
+                    emitter.onError(it.exception!!)
+            }
+        }
+    }
+
+    fun register(email: String, password: String) = Completable.create { emitter ->
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (!emitter.isDisposed) {
+                if (it.isSuccessful)
+                    emitter.onComplete()
+                else
+                    emitter.onError(it.exception!!)
+            }
+        }
+    }
+
+    fun logout() = firebaseAuth.signOut()
+
+    fun currentUser() = firebaseAuth.currentUser
+}
