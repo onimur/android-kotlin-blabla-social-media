9b2835007883f74b3af89ac97661f0389e6d8eb3
Murillo Comino <murillo_comino@hotmail.com>
2019-11-01 22:48:51 -0300

    Refactor methods and unit test creation to handle authentication errors

--- app/src/main/java/com/onimus/blablasocialmedia/mvvm/commons/AuthViewModel.kt
+++ app/src/main/java/com/onimus/blablasocialmedia/mvvm/commons/AuthViewModel.kt
@@ -14,38 +14,26 @@ package com.onimus.blablasocialmedia.mvvm.commons
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.FirebaseAuthException
 import com.onimus.blablasocialmedia.mvvm.data.repository.UserRepository
-import com.onimus.blablasocialmedia.mvvm.extensions.patternMatch
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_CREDENTIAL_ALREADY_IN_USE
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_CUSTOM_TOKEN_MISMATCH
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_EMAIL_ALREADY_IN_USE
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_INVALID_CREDENTIAL
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_INVALID_CUSTOM_TOKEN
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_INVALID_EMAIL
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_INVALID_USER_TOKEN
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_OPERATION_NOT_ALLOWED
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_REQUIRES_RECENT_LOGIN
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_USER_DISABLED
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_USER_MISMATCH
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_USER_NOT_FOUND
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_USER_TOKEN_EXPIRED
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_WEAK_PASSWORD
-import com.onimus.blablasocialmedia.mvvm.utils.AppConstants.ErrorFirebaseAuth.ERROR_WRONG_PASSWORD
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import androidx.lifecycle.MutableLiveData
-import com.onimus.blablasocialmedia.R
+import com.onimus.blablasocialmedia.mvvm.utils.HandleErrors
+import io.reactivex.Scheduler
 
 
-class AuthViewModel(private val repository: UserRepository) : ViewModel() {
+class AuthViewModel(
+    private val repository: UserRepository,
+    private val processScheduler: Scheduler = Schedulers.io(),
+    private val observerScheduler: Scheduler = AndroidSchedulers.mainThread()
+) : ViewModel() {
 
     var email: String? = null
     var password: String? = null
     var authListener: AuthListener? = null
+
+    private val handleErrors = HandleErrors()
 
     //disposable to dispose the Completable
     private val disposables = CompositeDisposable()
@@ -66,52 +54,66 @@ class AuthViewModel(private val repository: UserRepository) : ViewModel() {
     fun onClickButtonRegister() {
         authListener?.resetTextInputLayout()
         //validate email and password
-        val check = checkEmailAndPassword()
-        if (check) {
-            authListener?.resetTextInputLayout()
-            //if is valid then show progress
-            authListener?.showProgress()
-            //calling onRegisterClicked from repository to perform the actual authentication
-            val disposable = repository.onRegisterClicked(email!!, password!!)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    authListener?.hideProgress()
-                    authListener?.onSuccessAuth()
-                }, {
+        val checkEmail = handleErrors.checkEmail(email)
+        val checkPassword = handleErrors.checkPassword(password)
 
-                    authListener?.hideProgress()
-                    val error = getMessageError(it)
-                    //show message
-                    authListener?.onFailureAuth(error)
-                })
-            disposables.add(disposable)
+        //email and password is valid
+        when {
+            checkEmail.and(checkPassword) == -1 -> {
+                authListener?.resetTextInputLayout()
+                //if is valid then show progress
+                authListener?.showProgress()
+                //calling onRegisterClicked from repository to perform the actual authentication
+                val disposable = repository.onRegisterClicked(email!!, password!!)
+                    .subscribeOn(processScheduler)
+                    .observeOn(observerScheduler)
+                    .subscribe({
+                        authListener?.hideProgress()
+                        authListener?.onSuccessAuth()
+                    }, {
+
+                        authListener?.hideProgress()
+                        val error = handleErrors.getMessageError(it)
+                        //show message
+                        authListener?.onFailureAuth(error)
+                    })
+                disposables.add(disposable)
+            }
+            checkEmail != -1 -> authListener?.inEmailValidationError(checkEmail)
+            checkPassword != -1 -> authListener?.inPasswordValidationError(checkPassword)
         }
     }
 
     fun onClickButtonLogin() {
         authListener?.resetTextInputLayout()
         //validate email and password
-        val check = checkEmailAndPassword()
-        if (check) {
-            authListener?.resetTextInputLayout()
-            //if is valid then show progress
-            authListener?.showProgress()
-            //calling onLoginClicked from repository to perform the actual authentication
-            val disposable = repository.onLoginClicked(email!!, password!!)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    authListener?.hideProgress()
-                    authListener?.onSuccessAuth()
-                }, {
+        val checkEmail = handleErrors.checkEmail(email)
+        val checkPassword = handleErrors.checkPassword(password)
 
-                    authListener?.hideProgress()
-                    val error = getMessageError(it)
-                    //show message
-                    authListener?.onFailureAuth(error)
-                })
-            disposables.add(disposable)
+        //email and password is valid
+        when {
+            checkEmail.and(checkPassword) == -1 -> {
+                authListener?.resetTextInputLayout()
+                //if is valid then show progress
+                authListener?.showProgress()
+                //calling onLoginClicked from repository to perform the actual authentication
+                val disposable = repository.onLoginClicked(email!!, password!!)
+                    .subscribeOn(processScheduler)
+                    .observeOn(observerScheduler)
+                    .subscribe({
+                        authListener?.hideProgress()
+                        authListener?.onSuccessAuth()
+                    }, {
+
+                        authListener?.hideProgress()
+                        val error = handleErrors.getMessageError(it)
+                        //show message
+                        authListener?.onFailureAuth(error)
+                    })
+                disposables.add(disposable)
+            }
+            checkEmail != -1 -> authListener?.inEmailValidationError(checkEmail)
+            checkPassword != -1 -> authListener?.inPasswordValidationError(checkPassword)
         }
     }
 
@@ -123,53 +125,5 @@ class AuthViewModel(private val repository: UserRepository) : ViewModel() {
     fun onClickTextViewRegister() {
         //go to register_fragment
         authListener?.onNavigate()
-    }
-
-    private fun checkEmailAndPassword(): Boolean {
-        when {
-            email.isNullOrBlank() -> {
-                authListener?.inEmailValidationError(R.string.field_empty)
-                return false
-            }
-            !email!!.patternMatch(AppConstants.Pattern.EMAIL) -> {
-                authListener?.inEmailValidationError(R.string.error_invalid_email_format)
-                return false
-            }
-            password.isNullOrBlank() -> {
-                authListener?.inPasswordValidationError(R.string.field_empty)
-                return false
-            }
-            !password!!.patternMatch(AppConstants.Pattern.PASSWORD) -> {
-                authListener?.inPasswordValidationError(R.string.error_password_weak)
-                return false
-            }
-            else -> return true
-        }
-    }
-
-    private fun getMessageError(it: Throwable): Int {
-        if (it is FirebaseAuthException) {
-            return when (it.errorCode) {
-                ERROR_EMAIL_ALREADY_IN_USE -> R.string.error_email_already_use
-                ERROR_INVALID_EMAIL -> R.string.error_invalid_email_format
-                ERROR_WEAK_PASSWORD -> R.string.error_password_weak
-                ERROR_CREDENTIAL_ALREADY_IN_USE -> R.string.error_credentials_already_use
-                ERROR_INVALID_CUSTOM_TOKEN -> R.string.error_invalid_custom_token
-                ERROR_CUSTOM_TOKEN_MISMATCH -> R.string.error_custom_token_mismatch
-                ERROR_INVALID_CREDENTIAL -> R.string.error_invalid_credential
-                ERROR_WRONG_PASSWORD -> R.string.error_wrong_password
-                ERROR_USER_MISMATCH -> R.string.error_user_mismatch
-                ERROR_REQUIRES_RECENT_LOGIN -> R.string.error_requires_recent_login
-                ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL -> R.string.error_account_exists_with_different_credential
-                ERROR_USER_DISABLED -> R.string.error_user_disabled
-                ERROR_USER_TOKEN_EXPIRED -> R.string.error_user_token_expired
-                ERROR_USER_NOT_FOUND -> R.string.error_user_not_found
-                ERROR_INVALID_USER_TOKEN -> R.string.error_invalid_user_token
-                ERROR_OPERATION_NOT_ALLOWED -> R.string.error_operation_not_allowed
-                else -> R.string.error_unknown
-            }
-        } else {
-            return R.string.error_unknown
-        }
     }
 }
---
+++ app/src/test/java/com/onimus/blablasocialmedia/mvvm/utils/HandleErrorsTest.kt
@@ -0,0 +1,234 @@
+/*
+ *
+ *  * Created by Murillo Comino on 01/11/19 21:55
+ *  * Github: github.com/MurilloComino
+ *  * StackOverFlow: pt.stackoverflow.com/users/128573
+ *  * Email: murillo_comino@hotmail.com
+ *  *
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 01/11/19 21:55
+ *
+ */
+
+package com.onimus.blablasocialmedia.mvvm.utils
+
+import com.google.firebase.auth.*
+import com.onimus.blablasocialmedia.R
+import org.hamcrest.core.Is.`is`
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+
+class HandleErrorsTest {
+
+    companion object {
+        const val CORRECT_EMAIL = "mur@gmail.com"
+        const val CORRECT_PASSWORD = "Abc123"
+    }
+
+    private lateinit var handleErrors: HandleErrors
+
+    @Before
+    fun setUp() {
+        handleErrors = HandleErrors()
+    }
+
+    @Test
+    fun `checkEmail with empty or blank should return error`() {
+        assertThat(handleErrors.checkEmail(null), `is`(R.string.field_empty))
+        assertThat(handleErrors.checkEmail("  "), `is`(R.string.field_empty))
+    }
+
+    @Test
+    fun `checkPassword with empty or blank should return error`() {
+        assertThat(handleErrors.checkPassword(null), `is`(R.string.field_empty))
+        assertThat(handleErrors.checkPassword("  "), `is`(R.string.field_empty))
+    }
+
+    @Test
+    fun `checkEmail with invalid format should return error`() {
+        assertThat(
+            handleErrors.checkEmail("2s2s2sd2r2.com"),
+            `is`(R.string.error_invalid_email_format)
+        )
+        assertThat(
+            handleErrors.checkEmail("s2s @!dsq%@.gmail.com"),
+            `is`(R.string.error_invalid_email_format)
+        )
+
+    }
+
+    @Test
+    fun `checkPassword with invalid format should return error`() {
+        val error = R.string.error_password_weak
+
+        //without 1 character uppercase
+        assertThat(handleErrors.checkPassword("abc123"), `is`(error))
+        //without 1 numeric character
+        assertThat(handleErrors.checkPassword("abcdef"), `is`(error))
+        //without alphabetic characters
+        assertThat(handleErrors.checkPassword("123456"), `is`(error))
+        //without alphanumeric characters
+        assertThat(handleErrors.checkPassword("$%#@!&"), `is`(error))
+        //with less than 6 characters
+        assertThat(handleErrors.checkPassword("Abc12"), `is`(error))
+
+    }
+
+    @Test
+    fun `checkEmail and checkPassword with valid format should return -1`() {
+        assertThat(handleErrors.checkEmail(CORRECT_EMAIL), `is`(-1))
+        assertThat(handleErrors.checkPassword(CORRECT_PASSWORD), `is`(-1))
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun `getMessageError with FirebaseAuthException or to extend it should return error`() {
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthEmailException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_EMAIL_ALREADY_IN_USE,
+                    ""
+                )
+            ), `is`(R.string.error_email_already_use)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthActionCodeException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_INVALID_EMAIL,
+                    ""
+                )
+            ), `is`(R.string.error_invalid_email_format)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthEmailException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_WEAK_PASSWORD,
+                    ""
+                )
+            ), `is`(R.string.error_password_weak)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthInvalidCredentialsException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_CREDENTIAL_ALREADY_IN_USE,
+                    ""
+                )
+            ), `is`(R.string.error_credentials_already_use)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthRecentLoginRequiredException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_INVALID_CUSTOM_TOKEN,
+                    ""
+                )
+            ), `is`(R.string.error_invalid_custom_token)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthWebException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_CUSTOM_TOKEN_MISMATCH,
+                    ""
+                )
+            ), `is`(R.string.error_custom_token_mismatch)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_INVALID_CREDENTIAL,
+                    ""
+                )
+            ), `is`(R.string.error_invalid_credential)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_WRONG_PASSWORD,
+                    ""
+                )
+            ), `is`(R.string.error_wrong_password)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_USER_MISMATCH,
+                    ""
+                )
+            ), `is`(R.string.error_user_mismatch)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_REQUIRES_RECENT_LOGIN,
+                    ""
+                )
+            ), `is`(R.string.error_requires_recent_login)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL,
+                    ""
+                )
+            ), `is`(R.string.error_account_exists_with_different_credential)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_USER_DISABLED,
+                    ""
+                )
+            ), `is`(R.string.error_user_disabled)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_USER_TOKEN_EXPIRED,
+                    ""
+                )
+            ), `is`(R.string.error_user_token_expired)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_USER_NOT_FOUND,
+                    ""
+                )
+            ), `is`(R.string.error_user_not_found)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_INVALID_USER_TOKEN,
+                    ""
+                )
+            ), `is`(R.string.error_invalid_user_token)
+        )
+        assertThat(
+            handleErrors.getMessageError(
+                FirebaseAuthException(
+                    AppConstants.ErrorFirebaseAuth.ERROR_OPERATION_NOT_ALLOWED,
+                    ""
+                )
+            ), `is`(R.string.error_operation_not_allowed)
+        )
+        assertThat(
+            handleErrors.getMessageError(FirebaseAuthException(anyString(), "")),
+            `is`(R.string.error_unknown)
+        )
+    }
+
+    @Test
+    fun `getMessageError with other Exception should return unknown error`() {
+        assertThat(
+            handleErrors.getMessageError(ArrayStoreException("")),
+            `is`(R.string.error_unknown)
+        )
+        assertThat(
+            handleErrors.getMessageError(KotlinNullPointerException()),
+            `is`(R.string.error_unknown)
+        )
+    }
+}
