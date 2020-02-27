93dc03d7e2aa2887cc8b18f2403becc0e8ce9ce5
Murillo Comino <murillo_comino@hotmail.com>
2020-02-27 20:09:14 -0300

    Update layout

--- app/src/main/java/com/onimus/blablasocialmedia/mvvm/ui/resetpass/successsending/SuccessSendingEmailFragment.kt
+++ app/src/main/java/com/onimus/blablasocialmedia/mvvm/ui/resetpass/successsending/SuccessSendingEmailFragment.kt
@@ -1,6 +1,21 @@
 package com.onimus.blablasocialmedia.mvvm.ui.resetpass.successsending
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.onimus.blablasocialmedia.R
 
-class SuccessSendingEmailFragment : Fragment()
+class SuccessSendingEmailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_success_sending_email, container, false)
+    }
+    
+}
