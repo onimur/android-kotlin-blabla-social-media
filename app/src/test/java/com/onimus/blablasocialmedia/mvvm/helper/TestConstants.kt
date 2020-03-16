d1e2470e9bb0de45425bb86f0765d87c66972b60
Murillo Comino <murillo_comino@hotmail.com>
2020-03-16 18:43:36 -0300

    Added: Class of constants to use in tests

---
+++ app/src/test/java/com/onimus/blablasocialmedia/mvvm/helper/TestConstants.kt
@@ -0,0 +1,25 @@
+/*
+ *
+ *  * Created by Murillo Comino on 16/03/20 12:27
+ *  * Github: github.com/MurilloComino
+ *  * StackOverFlow: pt.stackoverflow.com/users/128573
+ *  * Email: murillo_comino@hotmail.com
+ *  *
+ *  * Copyright (c) 2020 . All rights reserved.
+ *  * Last modified 16/03/20 12:27
+ *
+ */
+
+package com.onimus.blablasocialmedia.mvvm.helper
+
+class TestConstants {
+
+    companion object {
+        const val UID = "id"
+        const val EMAIL = "abc@abc.com"
+        const val PASSWORD = "Abc123"
+        const val NAME = "José da Silva"
+    }
+
+
+}
